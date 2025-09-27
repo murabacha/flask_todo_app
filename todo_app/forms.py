66835a9d602d2aebce54b13fdca0c92edd7f868c
robert <murabacha@proton.me>
2025-09-27 16:44:52 +0300

    added some delete and edit todo functions and a user profile also

--- todo_app/forms.py
+++ todo_app/forms.py
@@ -1,7 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField,DateField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from wtforms import StringField, PasswordField, SubmitField, BooleanField,DateField,TextAreaField,FileField
+from wtforms.validators import  Optional, DataRequired, Length, Email, EqualTo, ValidationError
 from todo_app.models import User
+from flask_login import current_user
+from flask_wtf.file import FileField, FileAllowed
+
+
 
 
 class RegistrationForm(FlaskForm):
@@ -9,6 +13,7 @@ class RegistrationForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    image_file = FileField('Picture', validators=[Optional(), FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Sign Up')
 
     def validate_username(self, username):
@@ -32,7 +37,25 @@ class LoginForm(FlaskForm):
 
 class TodoForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
-    description = StringField('Description', validators=[DataRequired()])
+    description = TextAreaField('Description', validators=[DataRequired()])
     date_ending = DateField('Ending date',format='%Y-%m-%d', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
+class UpdateProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    image_file = FileField('Picture', validators=[Optional(), FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.')
--- todo_app/routes.py
+++ todo_app/routes.py
@@ -1,8 +1,11 @@
 from flask import render_template, redirect, url_for, flash, request
 from todo_app import app, db, bcrypt
 from todo_app.models import Todo,User
-from todo_app.forms import TodoForm,RegistrationForm, LoginForm
+from todo_app.forms import TodoForm,RegistrationForm, LoginForm,UpdateProfileForm
 from flask_login import login_required, current_user, login_user, logout_user,LoginManager
+from PIL import Image
+import os
+import secrets
  
  
 login_manager = LoginManager(app)
@@ -10,7 +13,7 @@ login_manager.login_view = 'login'
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
+                                                           
 @app.route('/')
 @app.route('/home')
 @login_required
@@ -50,18 +53,72 @@ def login():
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
+            flash('You have been logged in!', 'success')
             return redirect(next_page) if next_page else redirect(url_for('index'))
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
-@app.route('/delete')
+@app.route('/delete/<todo_id>')
 @login_required
-def delete():
-    todo = Todo.query.get(request.args.get('id'))
+def delete(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
     db.session.delete(todo)
     db.session.commit()
+    flash('Your todo has been deleted!', 'success')
     return redirect(url_for('index'))
+
+
+@app.route('/update/<todo_id>', methods=['GET', 'POST'])
+@login_required
+def update(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    form = TodoForm()
+    if form.validate_on_submit():
+        todo.title = form.title.data
+        todo.description = form.description.data
+        todo.date_ending = form.date_ending.data
+        db.session.commit()
+        flash('Your todo has been updated!', 'success')
+        return redirect(url_for('index'))
+    elif request.method == 'GET':
+        form.title.data = todo.title
+        form.description.data = todo.description
+        form.date_ending.data = todo.date_ending
+    return render_template('update.html', title='Update Todo', form=form)
+
+def save_picture(form_picture):
+    image_name = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = image_name + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+
+    output_size = (150, 150)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+@app.route('/profile' , methods=['GET', 'POST'])
+@login_required
+def profile():
+    form = UpdateProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        picture_name = save_picture(form.image_file.data)
+        current_image = current_user.image_file
+        if current_image != 'default.jpg':
+            os.remove(os.path.join(app.root_path, 'static/profile_pics', current_image))
+        current_user.image_file = picture_name
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('profile'))
+    if request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    return render_template('profile.html', title='Profile',form=form)
 @app.route('/logout')
 def logout():
     logout_user()
